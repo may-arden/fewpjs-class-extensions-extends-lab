1758e469eecb4777be1e9bfdc0ec3669f2e941d5
may arden <mayard3n@gmail.com>
2021-05-11 17:41:00 -0700

    commit tree 4b4206503d68855779de6db4326106dcfa4f48b1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+      this.count = this.sides.length; 
+    } 
+
+    get countSides() {
+        return this.sides.length; 
+    }
+
+    get perimeter() {
+        let sum = 0;
+        for (let int of this.sides) {
+            sum+= int 
+        }
+        return sum 
+    }
+}
+
+class Triangle extends Polygon {
+
+}
